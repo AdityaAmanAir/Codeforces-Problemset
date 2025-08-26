c7dd2ef6f7eb363203c02cba62d22e040d0bd9e4
AdityaAmanAir <amanaditya1947@proton.me>
2025-08-26 23:29:35 +0530

    Fixed Error in Solution 1

--- 791A  | Bear and Big Brother/Solution1_791A.cpp
+++ 791A  | Bear and Big Brother/Solution1_791A.cpp
@@ -2,9 +2,10 @@
 #include <cmath>
 int main(){
     double a;
-    int l,b;
+    double l,b;
     std::cin>>l>>b;
     a=(log(b)-log(l))/(log(3)-log(2));
+    if(a-int(a)>0.9999){a+=1;} // due to floating-point representation error : For example, a might be 2.0000000000000004 instead of exactly 2.0
     std::cout<<(int)a+1; // Since, It's guaranteed that Limak's weight is smaller than or equal to his brother's weight. Input like 6 4 (a>b) will not occure.
-    return 0;
+    return 0;//doing >0.9999 is not a good solution, using std::setprecision manipulator whould be better if the condition of the problem changes.
 }
